91f3572e70e1d8ba54dc2ecbd1832257b6131d9c
Sittinon Dormae <6810110373@psu.ac.th>
2025-12-17 00:55:10 +0700

    Phase3Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import NoteForm from "./component/NoteForm";
 import NoteList from "./component/NoteList";
-import { useState } from "react"; 
+import { useState,useEffect } from "react"; 
 import type { Note } from "./types";
 
  function App() {
-    const [notes,setNotes] = useState<Note[]>([]);
+    const [notes,setNotes] = useState<Note[]>(() => {
+        const saved = localStorage.getItem('notes');
+        if (saved) {
+            return JSON.parse(saved);
+        } else {
+            return []; // ถ้าไม่มีของเก่า ให้เริ่มด้วยค่าว่าง
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes]);
 
     const addNote = (text: string) =>{
         const newNote : Note ={
@@ -14,11 +25,15 @@ import type { Note } from "./types";
         setNotes([...notes,newNote]);
         console.log("Added:",newNote)
     };
+    const deleteNote= (id: number) => {
+        setNotes(notes.filter(note => note.id !== id));
+    };
+
     return (
         <div className="App">
             <h1>Test 1st Checkpoint</h1>
             <NoteForm onAdd={addNote}/>
-            <NoteList notes={notes}/>
+            <NoteList notes={notes} onDelete={deleteNote}/>
         </div>
     )
  }
--- src/component/NoteList.tsx
+++ src/component/NoteList.tsx
@@ -2,9 +2,10 @@ import type { Note } from "../types";
 
 interface NoteListProps {
   notes: Note[];
+  onDelete: (id: number) => void;
 }
 
-function NoteList({ notes }: NoteListProps) {
+function NoteList({ notes,onDelete }: NoteListProps) {
   return (
     <div className="note-list">
       <h3>Your Notes ({notes.length})</h3>
@@ -12,6 +13,7 @@ function NoteList({ notes }: NoteListProps) {
         {notes.map((note) => (
           <li key={note.id}>
             {note.text}
+            <button onClick={() => onDelete(note.id)}>Delete</button>
           </li>
         ))}
       </ul>
